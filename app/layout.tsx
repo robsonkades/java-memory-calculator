20b9ac78d1608b5985eee3aec291d877fb4f23a9
Robson Kades <robsonkades@outlook.com>
2024-11-08 20:42:14 -0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className="antialiased tracking-tight">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
