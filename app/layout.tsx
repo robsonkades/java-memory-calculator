393873c5c8925a0f40d3f12755ee3777f001fd68
Robson Kades <robsonkades@outlook.com>
2024-11-12 23:15:02 -0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,12 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://jvm.expert'),
+  applicationName: 'JVM Expert',
+
+  keywords: 'JVM configuration tool, Java JVM optimization, Optimize JVM performance, Java memory management, Java heap configuration, JVM tuning guide, Java performance tuning, JVM memory calculator, Configure JVM settings, JVM optimization tips, Java application performance, JVM Expert tool, JVM best practices, Open source JVM tool, JVM parameters guide, Java garbage collection tuning, Java virtual machine performance, Java JVM recommendations, JVM optimization for production, Java startup performance, Optimize JVM heap size, Java tuning for large applications, Java application optimization, Java performance tips, Java memory allocation guide',
   verification: {
-    google: "AzZT5Jz7T3cIN8WQK_b8nCmwDtCthcDOSE9SZD5hu48"
+    google: "AzZT5Jz7T3cIN8WQK_b8nCmwDtCthcDOSE9SZD5hu48",
+    yandex: "6f41a5cba5e05adb",
   },
   alternates: {
     canonical: '/',
